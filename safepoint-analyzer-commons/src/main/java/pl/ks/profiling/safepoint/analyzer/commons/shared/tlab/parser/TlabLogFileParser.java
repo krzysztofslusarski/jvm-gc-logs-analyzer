17c9cbaebfb31ac0d733830bd7c9a5f704bf3340
Artur Owczarek <a.owczarek@adform.com>
2020-12-20 11:06:46 +0100

    Add missing license preambules

--- safepoint-analyzer-commons/src/main/java/pl/ks/profiling/safepoint/analyzer/commons/shared/tlab/parser/TlabLogFileParser.java
+++ safepoint-analyzer-commons/src/main/java/pl/ks/profiling/safepoint/analyzer/commons/shared/tlab/parser/TlabLogFileParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Krzysztof Slusarski
+ * Copyright 2020 Krzysztof Slusarski, Artur Owczarek
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
