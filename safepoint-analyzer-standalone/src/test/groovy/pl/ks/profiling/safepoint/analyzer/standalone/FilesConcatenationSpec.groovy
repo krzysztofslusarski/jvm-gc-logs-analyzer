d97d0a3ce00d06882dc8c654eb338db27bb3bd53
Artur Owczarek <a.owczarek@adform.com>
2020-12-22 16:52:48 +0100

    Complete tests for files concatenation

--- safepoint-analyzer-standalone/src/test/groovy/pl/ks/profiling/safepoint/analyzer/standalone/FilesConcatenationSpec.groovy
+++ safepoint-analyzer-standalone/src/test/groovy/pl/ks/profiling/safepoint/analyzer/standalone/FilesConcatenationSpec.groovy
@@ -18,8 +18,12 @@ package pl.ks.profiling.safepoint.analyzer.standalone
 
 import spock.lang.Specification
 
+import java.nio.file.Files
+import java.util.stream.Collectors
+
 class FilesConcatenationSpec extends Specification {
-    def "Should sort files with first timestamp in file"() {
+
+    def "should sort files with first timestamp in file"() {
         given:
         File file1 = getFile("./concatFile.3.log")
         File file2 = getFile("./concatFile.4.log")
@@ -30,10 +34,12 @@ class FilesConcatenationSpec extends Specification {
 
         def approach1 = [file1, file2, file3, file4, file5, file6] as File[]
         def approach2 = [file6, file5, file4, file3, file2, file1] as File[]
+        def approach3 = [file6, file5, file4, file3, file2, file1] as File[]
 
         when:
         def sorted1 = FilesConcatenation.sortByTimestamp(approach1)
         def sorted2 = FilesConcatenation.sortByTimestamp(approach2)
+        def sorted3 = FilesConcatenation.sortByTimestamp(approach3)
 
         then:
         sorted1[0] == file1
@@ -49,10 +55,48 @@ class FilesConcatenationSpec extends Specification {
         sorted2[3] == file4
         sorted2[4] == file5
         sorted2[5] == file6
+
+        sorted3[0] == file1
+        sorted3[1] == file2
+        sorted3[2] == file3
+        sorted3[3] == file4
+        sorted3[4] == file5
+        sorted3[5] == file6
     }
 
-    private File getFile(String s) {
-        return new File(getClass().getResource(s).toURI())
+    def "should concatenate files"() {
+        when:
+        String concatenated = concatenateFiles("fileWithoutNewLine.log", "nextFile.log")
+
+        then:
+        concatenated == """file without new line
+some next file"""
     }
 
+    def "should keep new lines while concatenating"() {
+        when:
+        String concatenated = concatenateFiles("fileWithNewLine.log", "nextFile.log")
+
+        then:
+        concatenated == """file with new line
+
+some next file"""
+    }
+
+    private static File getFile(String s) {
+        return new File(FilesConcatenationSpec.class.getClassLoader().getResource(s).toURI())
+    }
+
+    private static File createTemporaryFile() {
+        File file = File.createTempFile("jvm-gc-logs-analyzer-tests-", "-filesConcatenationSpec.tmp")
+        file.deleteOnExit()
+        return file;
+    }
+
+    private static String concatenateFiles(String... filesPath) {
+        File outputFile = createTemporaryFile()
+        List<File> files = (filesPath as List<String>).collect{getFile(it)}
+        FilesConcatenation.concatenate(files, outputFile)
+        return Files.lines(outputFile.toPath()).collect(Collectors.toList()).join("\n")
+    }
 }
