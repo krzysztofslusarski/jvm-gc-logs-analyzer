0908a40f481be3aa1ced8f2075caf1af32797b4d
Krzysztof Ślusarski <pasqal@gmail.com>
2021-07-19 19:53:40 +0200

    Genuine / not genuine

--- safepoint-analyzer-commons/src/main/java/pl/ks/profiling/safepoint/analyzer/commons/shared/gc/page/GCHeapAfter.java
+++ safepoint-analyzer-commons/src/main/java/pl/ks/profiling/safepoint/analyzer/commons/shared/gc/page/GCHeapAfter.java
@@ -15,37 +15,74 @@
  */
 package pl.ks.profiling.safepoint.analyzer.commons.shared.gc.page;
 
-import pl.ks.profiling.gui.commons.Chart;
-import pl.ks.profiling.gui.commons.Page;
-import pl.ks.profiling.safepoint.analyzer.commons.shared.report.JvmLogFile;
-import pl.ks.profiling.safepoint.analyzer.commons.shared.PageCreator;
-import pl.ks.profiling.safepoint.analyzer.commons.shared.PageUtils;
-import pl.ks.profiling.safepoint.analyzer.commons.shared.gc.parser.GCLogCycleEntry;
-
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import pl.ks.profiling.gui.commons.Chart;
+import pl.ks.profiling.gui.commons.Page;
+import pl.ks.profiling.gui.commons.PageContent;
+import pl.ks.profiling.safepoint.analyzer.commons.shared.PageCreator;
+import pl.ks.profiling.safepoint.analyzer.commons.shared.PageUtils;
+import pl.ks.profiling.safepoint.analyzer.commons.shared.gc.parser.GCLogCycleEntry;
+import pl.ks.profiling.safepoint.analyzer.commons.shared.report.JvmLogFile;
 
 public class GCHeapAfter implements PageCreator {
     @Override
     public Page create(JvmLogFile jvmLogFile, DecimalFormat decimalFormat) {
+        boolean hasNotGenuine = jvmLogFile.getGcLogFile().getCycleEntries()
+                .stream()
+                .anyMatch(gcLogCycleEntry -> !gcLogCycleEntry.isGenuineCollection());
+        boolean hasToSpace = jvmLogFile.getGcLogFile().getCycleEntries()
+                .stream()
+                .anyMatch(GCLogCycleEntry::isWasToSpaceExhausted);
+
+        List<PageContent> charts = new ArrayList<>(3);
+        if (hasNotGenuine) {
+            charts.add(
+                    Chart.builder()
+                            .chartType(Chart.ChartType.LINE)
+                            .title("Heap after GC - genuine collections")
+                            .info("Collections that are not \"genuine\": remark, cleanup, all piggybacks, initiated by too many humongous objects")
+                            .xAxisLabel("Seconds since application start")
+                            .yAxisLabel("MB")
+                            .xAxisColumnIndex(chartColumns.indexOf(TIMESTAMP_COLUMN))
+                            .data(getHeapAfterGCSizeChart(jvmLogFile))
+                            .build()
+            );
+        }
+        charts.add(
+                Chart.builder()
+                        .chartType(Chart.ChartType.LINE)
+                        .title("Heap after GC - all collections")
+                        .xAxisLabel("Seconds since application start")
+                        .yAxisLabel("MB")
+                        .xAxisColumnIndex(chartColumns.indexOf(TIMESTAMP_COLUMN))
+                        .data(getHeapAfterGCAllCollectionsSizeChart(jvmLogFile))
+                        .build()
+        );
+
+        if (hasToSpace) {
+            charts.add(
+                    Chart.builder()
+                            .chartType(Chart.ChartType.LINE)
+                            .title("Heap after GC - genuine collections without to-space exhausted")
+                            .xAxisLabel("Seconds since application start")
+                            .yAxisLabel("MB")
+                            .xAxisColumnIndex(chartColumns.indexOf(TIMESTAMP_COLUMN))
+                            .data(getHeapAfterGCWithoutToSpaceExhaustedSizeChart(jvmLogFile))
+                            .build()
+            );
+        }
+
         return Page.builder()
                 .menuName("Heap after GC")
                 .fullName("Heap after GC")
                 .info("These charts presents heap size after Garbage Collection. There are displayed only young, mixed and full collections. " +
                         "There are not displayed any piggybacked and concurrent collections.")
                 .icon(Page.Icon.CHART)
-                .pageContents(List.of(
-                        Chart.builder()
-                                .chartType(Chart.ChartType.LINE)
-                                .title("Heap after GC")
-                                .xAxisLabel("Seconds since application start")
-                                .yAxisLabel("MB")
-                                .xAxisColumnIndex(chartColumns.indexOf(TIMESTAMP_COLUMN))
-                                .data(getHeapAfterGCSizeChart(jvmLogFile))
-                                .build()
-                ))
+                .pageContents(charts)
                 .build();
     }
 
@@ -65,6 +102,21 @@ public class GCHeapAfter implements PageCreator {
         List<GCLogCycleEntry> cyclesToShow = jvmLogFile.getGcLogFile().getCycleEntries()
                 .stream()
                 .filter(GCLogCycleEntry::isGenuineCollection)
+                .collect(Collectors.toList());
+
+        return PageUtils.toMatrix(cyclesToShow, chartColumns, chartExtractors);
+    }
+
+    private static Object[][] getHeapAfterGCAllCollectionsSizeChart(JvmLogFile jvmLogFile) {
+        List<GCLogCycleEntry> cyclesToShow = jvmLogFile.getGcLogFile().getCycleEntries();
+        return PageUtils.toMatrix(cyclesToShow, chartColumns, chartExtractors);
+    }
+
+    private static Object[][] getHeapAfterGCWithoutToSpaceExhaustedSizeChart(JvmLogFile jvmLogFile) {
+        List<GCLogCycleEntry> cyclesToShow = jvmLogFile.getGcLogFile().getCycleEntries()
+                .stream()
+                .filter(GCLogCycleEntry::isGenuineCollection)
+                .filter(gcLogCycleEntry -> !gcLogCycleEntry.isWasToSpaceExhausted())
                 .collect(Collectors.toList());
 
         return PageUtils.toMatrix(cyclesToShow, chartColumns, chartExtractors);
--- safepoint-analyzer-commons/src/main/java/pl/ks/profiling/safepoint/analyzer/commons/shared/gc/page/GCHeapBefore.java
+++ safepoint-analyzer-commons/src/main/java/pl/ks/profiling/safepoint/analyzer/commons/shared/gc/page/GCHeapBefore.java
@@ -16,36 +16,59 @@
 package pl.ks.profiling.safepoint.analyzer.commons.shared.gc.page;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import pl.ks.profiling.gui.commons.Chart;
 import pl.ks.profiling.gui.commons.Page;
-import pl.ks.profiling.safepoint.analyzer.commons.shared.report.JvmLogFile;
+import pl.ks.profiling.gui.commons.PageContent;
 import pl.ks.profiling.safepoint.analyzer.commons.shared.PageCreator;
 import pl.ks.profiling.safepoint.analyzer.commons.shared.PageUtils;
 import pl.ks.profiling.safepoint.analyzer.commons.shared.gc.parser.GCLogCycleEntry;
+import pl.ks.profiling.safepoint.analyzer.commons.shared.report.JvmLogFile;
 
 public class GCHeapBefore implements PageCreator {
     @Override
     public Page create(JvmLogFile jvmLogFile, DecimalFormat decimalFormat) {
+        boolean hasNotGenuine = jvmLogFile.getGcLogFile().getCycleEntries()
+                .stream()
+                .anyMatch(gcLogCycleEntry -> !gcLogCycleEntry.isGenuineCollection());
+        List<PageContent> charts = new ArrayList<>(3);
+        if (hasNotGenuine) {
+            charts.add(
+                    Chart.builder()
+                            .chartType(Chart.ChartType.POINTS_OR_LINE)
+                            .seriesTypes(new Chart.SeriesType[] {Chart.SeriesType.POINTS, Chart.SeriesType.LINE})
+                            .title("Heap before GC - genuine collections")
+                            .info("Collections that are not \"genuine\": remark, cleanup, all piggybacks, initiated by too many humongous objects")
+                            .xAxisLabel("Seconds since application start")
+                            .yAxisLabel("MB")
+                            .xAxisColumnIndex(chartColumns.indexOf(TIMESTAMP_COLUMN))
+                            .data(getHeapBeforeGCSizeChart(jvmLogFile))
+                            .build()
+            );
+        }
+
+        charts.add(
+                Chart.builder()
+                        .chartType(Chart.ChartType.POINTS_OR_LINE)
+                        .seriesTypes(new Chart.SeriesType[]{Chart.SeriesType.POINTS, Chart.SeriesType.LINE})
+                        .title("Heap before GC - all collections")
+                        .xAxisLabel("Seconds since application start")
+                        .yAxisLabel("MB")
+                        .xAxisColumnIndex(chartColumns.indexOf(TIMESTAMP_COLUMN))
+                        .data(getHeapBeforeGCAllCollectionsSizeChart(jvmLogFile))
+                        .build()
+        );
+
         return Page.builder()
                 .menuName("Heap before GC")
                 .fullName("Heap before GC")
                 .info("These charts presents heap size before Garbage Collection. There are displayed only young, mixed and full collections. " +
                         "There are not displayed any piggybacked and concurrent collections.")
                 .icon(Page.Icon.CHART)
-                .pageContents(List.of(
-                        Chart.builder()
-                                .chartType(Chart.ChartType.POINTS_OR_LINE)
-                                .seriesTypes(new Chart.SeriesType[] {Chart.SeriesType.POINTS, Chart.SeriesType.LINE})
-                                .title("Heap before GC")
-                                .xAxisLabel("Seconds since application start")
-                                .yAxisLabel("MB")
-                                .xAxisColumnIndex(chartColumns.indexOf(TIMESTAMP_COLUMN))
-                                .data(getHeapBeforeGCSizeChart(jvmLogFile))
-                                .build()
-                ))
+                .pageContents(charts)
                 .build();
     }
 
@@ -67,6 +90,11 @@ public class GCHeapBefore implements PageCreator {
                 .filter(GCLogCycleEntry::isGenuineCollection)
                 .collect(Collectors.toList());
 
+        return PageUtils.toMatrix(cyclesToShow, chartColumns, chartExtractors);
+    }
+
+    private static Object[][] getHeapBeforeGCAllCollectionsSizeChart(JvmLogFile jvmLogFile) {
+        List<GCLogCycleEntry> cyclesToShow = jvmLogFile.getGcLogFile().getCycleEntries();
         return PageUtils.toMatrix(cyclesToShow, chartColumns, chartExtractors);
     }
 }
