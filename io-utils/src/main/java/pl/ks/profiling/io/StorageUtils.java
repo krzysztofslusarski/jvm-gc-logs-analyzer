e1ff92d3224f1a843192c5d6c0dcace46a1a2852
Krzysztof Ślusarski <pasqal@gmail.com>
2021-08-03 17:02:01 +0200

    #25 - add "/" for upload plain text

--- io-utils/src/main/java/pl/ks/profiling/io/StorageUtils.java
+++ io-utils/src/main/java/pl/ks/profiling/io/StorageUtils.java
@@ -15,7 +15,11 @@
  */
 package pl.ks.profiling.io;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.UUID;
 import lombok.experimental.UtilityClass;
 import org.apache.poi.util.IOUtils;
@@ -45,7 +49,7 @@ public class StorageUtils {
     }
 
     public String savePlainText(String dir, String text) throws IOException {
-        String savedFileName = dir + UUID.randomUUID().toString() + "plain-text.log";
+        String savedFileName = dir + "/" + UUID.randomUUID().toString() + "plain-text.log";
         try (PrintWriter out = new PrintWriter(savedFileName)) {
             out.println(text);
         }
